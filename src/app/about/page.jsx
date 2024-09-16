8146079824a3eddb6e18608fbbded409f0a5e647
Shahnawaz Sazid <shahnawazsazid60@gmail.com>
2024-09-16 23:03:42 +0600

    Pre Final Forr Deployment

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -4,12 +4,12 @@ const getTime = async () => {
     // Cache the na diye updated ta dibe proti refresh e notun data dibe mane network request korbe 
     // Backend theke o kora jay eta
 
-    // const res = await fetch(`http://localhost:3000/time`, {cache :'no-store'})
+    const res = await fetch(`http://localhost:3000/time`, {cache :'no-store'})
 
     // kisukhon por por re validate korbe
     // const res = await fetch(`http://localhost:3000/time`, { next: { revalidate: 5 } })
-    // const data = await res.json()
-    // return data.currentTime
+    const data = await res.json()
+    return data.currentTime
 }
 
 const page = async () => {
@@ -19,7 +19,7 @@ const page = async () => {
     return (
         <div>
             <h1>About Page</h1>
-            {/* <h2>Time : {currentTime}</h2> */}
+            <h2>Time : {currentTime}</h2>
         </div>
     );
 };
