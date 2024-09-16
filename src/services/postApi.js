39851ddd7ff3c8ffa76e93db094d4f4cb82a0f52
Shahnawaz Sazid <shahnawazsazid60@gmail.com>
2024-09-16 12:55:20 +0600

    Redirecting Mrhod 1 in Next.js

--- src/services/postApi.js
+++ src/services/postApi.js
@@ -1,5 +1,5 @@
 export const getPost = async () => {
     const res = await fetch("https://jsonplaceholder.typicode.com/posts")
     const data = await res.json()
-    return data
-}
+    return data;
+};
